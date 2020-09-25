d3751e0273c87d39153db5dca61da7d19d096f85
HoangTrongBaoAnh <biminhiabo2@gmail.com>
2020-09-25 21:49:45 +0700

    .

---
+++ CURA test project/Test Suites/Test suite.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>lab01</description>
+   <name>Test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>538cd29d-d2f5-423f-98c7-ea53a840508a</testSuiteGuid>
+   <testCaseLink>
+      <guid>5abb642b-97a6-4131-ae7f-5d499c41d849</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test case 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d32dbd9e-82e4-490e-a89c-508763889e85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test case 2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>896684a8-f733-4be2-8641-1834aacd2a85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test case 4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c935945d-0c34-4d84-b175-e811a51d03e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test case 3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cd440fd5-fd1e-4737-82e0-7c82d54e7759</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test case 5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
